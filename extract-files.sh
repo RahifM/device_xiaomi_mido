LineageOS/android_device_xiaomi_mido

mido: Add seperate list for common qcom blobs
Change-Id: I67a514ac21b52bfbb223b848fc464d73dbe79697

extract-files.sh
@@ -53,5 +53,6 @@ fi
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
+extract "$MY_DIR"/proprietary-file-qc.txt "$SRC"
 
 "$MY_DIR"/setup-makefiles.sh

RahifM/device_xiaomi_mido

extract-files.sh
@@ -53,5 +53,6 @@ fi
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
+extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
 
 "$MY_DIR"/setup-makefiles.sh