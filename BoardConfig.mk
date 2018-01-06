5ac05ca1e8b3ce9069eaddc811c1ca0363c0394a
RahifM <faizel326@gmail.com>
2018-01-06 19:10:26 +0000

    mido: Drop lineagehw
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,10 +177,6 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_mido
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
-# Lineage Hardware
-BOARD_HARDWARE_CLASS += \
-    $(DEVICE_PATH)/lineagehw
-
 #Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
