97df8418406e2deb7202cee298c0a6edc7b9f598
RahifM <faizel326@gmail.com>
2018-10-21 22:42:10 +0530

    IM DUMB AF !!
    Signed-off-by: RahifM <faizel326@gmail.com>

--- omni_mido.mk
+++ omni_mido.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
