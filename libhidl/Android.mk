12fe18d2bf0a6ccce349b0c06c8f8dce63d7c41f
RahifM <faizel326@gmail.com>
2019-07-26 03:03:55 +0530

    These arent needed as we've proper workaround in rom source.
    Signed-off-by: RahifM <faizel326@gmail.com>

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -15,7 +15,7 @@
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := libhidltransport
-LOCAL_MODULE := android.hidl.base@1.0
+LOCAL_MODULE := android.hidl.base-1.0-java
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
