27542e1a2443e563bf1aa2d6b6d8bc0566ffffab
RahifM <faizel326@gmail.com>
2019-08-27 00:51:02 +0530

    Should've added this long back when bringup was done :3
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,6 +203,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 VENDOR_SECURITY_PATCH := 2019-04-01
 
 # SELinux
+include vendor/omni/sepolicy/sepolicy.mk
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
