c04785419d8e62c4ac87cfe7ec4fa66586b4d724
Robert Shih <robertshih@google.com>
2019-11-25 22:25:41 -0800

    mido: Enable clearkey drm plugin v1.3
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: I7453b035ca29c7ee889d03de0d55e6d947b3cb77
    Signed-off-by: drk <drk>

--- device.mk
+++ device.mk
@@ -173,8 +173,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.3-service-lazy.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
