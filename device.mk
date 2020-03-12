9775373f247b988786a20b6d72733ad6328ad63c
RiteshSaxena <riteshsax007@gmail.com>
2020-03-12 00:35:22 +0000

    mido: Restore hostapd configs
    * CAF required
    
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>
    Signed-off-by: RahifM <faizel326@gmail.com>

--- device.mk
+++ device.mk
@@ -408,6 +408,7 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
     libqmiservices_shim \
+    wifilogd \
     libwifi-hal-qcom \
     wpa_supplicant \
     wpa_supplicant.conf
@@ -416,6 +417,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/hostapd_default.conf:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/wifi/hostapd.deny:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.deny
 
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
