8cc947b40d062c4621c6cde969c3de52443b1c4d
RahifM <faizel326@gmail.com>
2020-05-23 00:34:28 +0530

    mido: Add prebuilt kernel
    Signed-off-by: RahifM <faizel326@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,14 @@
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+    LOCAL_KERNEL := device/xiaomi/mido/prebuilt/Image.gz-dtb
+else
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
