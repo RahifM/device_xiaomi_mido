639afad392007ef141cda086ac59b55037e18084
RahifM <faizel326@gmail.com>
2020-05-23 00:34:28 +0530

    prebuilt kernel
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,8 +47,11 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_SOURCE := kernel/xiaomi/mido
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+ TARGET_KERNEL_CLANG_COMPILE := true
+ TARGET_KERNEL_SOURCE := kernel/xiaomi/mido
+endif
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
