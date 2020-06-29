44b187363a2d0c2f07f95d8462ba45aeb4f8e404
RahifM <faizel326@gmail.com>
2020-06-29 17:43:33 +0530

    mido: Move to PA Sepolicy
     * Also some address denials
    
       pulled/ reffered from:
       https://github.com/AOSPA/android_device_xiaomi_rosy/commit/2b1c5545e6313e474ffaf19c87a0734d0eb4e4c5#diff-00d5845369f573c3c40853c599084b2c
       https://github.com/AOSPA/android_device_oneplus_oneplus3/commit/41e4936308a842c6eecd75ee0d8fe871cf0d6372#diff-00d5845369f573c3c40853c599084b2c
    
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,7 +200,6 @@ TARGET_USES_OLD_MNC_FORMAT := true
 VENDOR_SECURITY_PATCH := 2020-01-01
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
