198a5cf8be14895cc65b5dbfa165ffc77537dc9e
RahifM <faizel326@gmail.com>
2020-07-23 08:00:32 +0000

    mido: lineage -> du
    Signed-off-by: RahifM <faizel326@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mido.mk
+    $(LOCAL_DIR)/du_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mido-user \
-    lineage_mido-userdebug \
-    lineage_mido-eng
+    du_mido-user \
+    du_mido-userdebug \
+    du_mido-eng
