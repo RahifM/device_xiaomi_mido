f7df1f3f6f0334674efa12e671647675eafbc830
any <any>
2020-08-18 18:36:41 +0000

    citrus bringup
    Signed-off-by: any <any>

--- citrus_mido.mk
+++ citrus_mido.mk
@@ -28,7 +28,7 @@ AOSP_BUILD_TYPE := DepressionAndObsession
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := aosp_mido
+PRODUCT_NAME := citrus_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
