ea96202782fb34470ffd82a09cee9146ec012773
drk <drk>
2020-09-23 05:39:03 +0000

    wip
    Signed-off-by: drk <drk>

--- device.mk
+++ device.mk
@@ -442,10 +442,8 @@ PRODUCT_HOST_PACKAGE += \
       signapk
 
 PRODUCT_COPY_FILES += \
-      prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libprotobuf-cpp-full-v29.so \
-      prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libprotobuf-cpp-lite-v29.so \
-      prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
-      prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+      prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+      prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
 
-PRODUCT_EXTRA_VNDK_VERSIONS := 29
-PRODUCT_TARGET_VNDK_VERSION := 29
+#PRODUCT_EXTRA_VNDK_VERSIONS := 29
+#PRODUCT_TARGET_VNDK_VERSION := 29
