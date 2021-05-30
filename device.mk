580d9e33a5809f5b3691b2b3cbce034c11de5de2
RahifM <faizel326@gmail.com>
2021-05-30 17:07:49 +0530

    mido: Cleanup
    Signed-off-by: RahifM <rahifmanjatha372@gmail.com>

--- device.mk
+++ device.mk
@@ -262,10 +262,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_mido
 
-# LiveDisplay
-#PRODUCT_PACKAGES += \
-#    vendor.lineage.livedisplay@2.0-service-sdm
-
 #ifeq ($(TARGET_BUILD_VARIANT),userdebug)
 # Matlog
 PRODUCT_PACKAGES += \
@@ -386,14 +382,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
-# Touch features
-#PRODUCT_PACKAGES += \
-#    vendor.lineage.touch@1.0-service.xiaomi_mido
-
-# Trust HAL
-#PRODUCT_PACKAGES += \
-#   vendor.lineage.trust@1.0-service
 
 # TimeKeep
 PRODUCT_PACKAGES += \
