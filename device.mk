fe72811de8b55a123439b168cd368cce821e9fa3
RahifM <faizel326@gmail.com>
2021-06-07 17:34:02 +0530

    Revert "Build vendor.qti.hardware.perf@2.1 from source"
    This reverts commit 3819280a3a0222c6e3c9530830c98733b5d4cf0c.

--- device.mk
+++ device.mk
@@ -299,8 +299,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.1.vendor
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
