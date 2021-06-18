3cee7b4b6fc985adbd1cb6850bec606af1fd8b90
RahifM <rahifmanjatha372@gmail.com>
2021-06-19 01:17:37 +0530

    Back to los pathmaps
    Signed-off-by: RahifM <rahifmanjatha372@gmail.com>

--- device.mk
+++ device.mk
@@ -38,10 +38,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    hardware/qcom/display-caf-msm8996 \
-    hardware/qcom/audio-caf-msm8996 \
-    hardware/qcom/media-caf-msm8996
+    $(LOCAL_PATH)
 
 # Permissions
 PRODUCT_COPY_FILES += \
