7f3f9f88eba146c7ce2f9744415ebe13d5968a56
RahifM <rahifmanjatha372@gmail.com>
2021-06-18 16:35:27 +0530

    Fix kernel updater script
    Signed-off-by: RahifM <rahifmanjatha372@gmail.com>

--- prebuiltkernelupdate.sh
+++ prebuiltkernelupdate.sh
@@ -2,4 +2,4 @@
 
 cd k*/xi*/m*
 ./build.sh
-cp -r $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/../../../de*/xi*/m*/pre*
+cp -r $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/../../../de*/xi*/m*/prebuilt
