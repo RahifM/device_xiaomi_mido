6cb961e28b0c4180fc0cfbd86f9b2e3d842bc570
RahifM <rahifmanjatha372@gmail.com>
2021-07-07 08:20:06 +0000

    Add tgup and update build script
    Signed-off-by: RahifM <rahifmanjatha372@gmail.com>

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #export WITH_GMS=true
 
 # Compile the build
-. build/envsetup.sh && lunch mido-userdebug && make -j8 otapackage 2>&1 | tee buildlog.txt
+. bu*/env* && lunch mido-userdebug && make -j8 otapackage 2>&1 | tee "buildlog-$(date +'%m%d-%H%M').txt"
