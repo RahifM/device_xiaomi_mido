e75727591ea9bdee1c8a4206d29802a834f4e767
RahifM <rahifmanjatha372@gmail.com>
2021-07-08 16:35:06 +0000

    Add tgsh and update buildsh
    Signed-off-by: RahifM <rahifmanjatha372@gmail.com>

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #export WITH_GMS=true
 
 # Compile the build
-. build/envsetup.sh && lunch mido-userdebug && make -j8 otapackage 2>&1 | tee buildlog.txt
+. build/envsetup.sh && lunch potato_mido-userdebug && mka potato 2>&1 | tee "buildlog-$(date +'%m%d-%H%M').txt"
