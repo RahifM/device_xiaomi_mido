d92f8b3eb7022854ba46c1b09caaa1b084cedf25
RahifM <rahifmanjatha372@gmail.com>
2021-07-25 22:14:12 +0530

    Rename tgup to upload
    Signed-off-by: RahifM <rahifmanjatha372@gmail.com>

--- tgup.sh
+++
@@ -1,6 +0,0 @@
-gdrive upload --share out/target/product/mido/pot*dum*zip | tail -1 | cut -d " " -f 7 > up.txt
-pkgname=$(ls o*/t*/p*/m*/p*dum*zip | cut -c 25-84)
-up=$(grep http* up.txt)
-bash ~/tele*/tele* -M -f buildlog*.txt "[${pkgname}](${up})"
-rm buildlog*.txt
-rm -rf out/target/product/mido/pot*dum*zip
