a8bc97156aa34b30d768937979fda2d828509bba
RahifM <rahifmanjatha372@gmail.com>
2021-07-25 12:22:30 +0530

    Move to user builds
    Signed-off-by: RahifM <rahifmanjatha372@gmail.com>

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #export WITH_GMS=true
 
 # Compile the build
-. bu*/env* && lunch hentai_mido-userdebug && make -j16 otapackage 2>&1 | tee "buildlog-$(date +'%m%d-%H%M').txt"
+. bu*/env* && lunch hentai_mido-user && make -j16 otapackage 2>&1 | tee "buildlog-$(date +'%m%d-%H%M').txt"
